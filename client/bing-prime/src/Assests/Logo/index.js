62e6ced72d613a636c0b0de068a1f1f7dfac6b17
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-09-24 00:51:50 +0530

    client

---
+++ client/bing-prime/src/Assests/Logo/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Logo.css';
+
+export default function Logo() {
+  return (
+    <div class="logo">
+  <span>B</span>
+  <span>I</span>
+  <span>N</span>
+  <span>G</span>
+  </div>
+  )
+}
