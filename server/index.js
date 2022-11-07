0cbb3a61bd634bb84b470b9980b231e20744afb8
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-11-08 00:39:53 +0530

    API for login-signup page

---
+++ server/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const port = 3001;
+const bing = express();
+const cors = require("cors");
+const connectToDB = require("./db");
+const userModel = require("./models/user.model");
+
+connectToDB();
+bing.use(express.json());
+bing.use(cors());
+
+bing.listen(port, console.log("server is listening on PORT - ", port));
+bing.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    const existUser = await userModel.findOne({ email: email });
+    if (existUser) {
+        res.send({
+            message: "user already exists"
+        })
+    }
+
+    else {
+        const newUser = new userModel({
+            name, email, password
+        })
+        newUser.save(err => {
+            if (err) {
+                res.send(err)
+            }
+            else {
+                res.send({
+                    message: "Account created successfully"
+                })
+            }
+
+        })
+    }
+})
+
+bing.post('/login', async (req, res) => {
+    const {email, password} = req.body
+    const user = await userModel.findOne({email : email})
+    console.log(user);
+
+    if(!user){
+        return res.send({
+            message : "user does not exist"
+        })
+    }
+
+    if(password !== user.password) {
+        return res.send({
+            message : "incorrect password"
+        })
+    }
+
+    res.send({
+        message : "login successfully",
+        user
+    })
+})
